1cc11e3b6b2d6102c29dbc9559015b9bdc5768f5
Michael Coetzee <michaelc@tsn.systems>
2025-08-06 23:13:21 +0200

    Add ACR and ACRPull role for kublet identity

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -1,3 +1,14 @@
+data "azurerm_container_registry" "acr" {
+  name                = "demoacr1754512503"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.name
   location            = var.location
